bfa582c0a96d3c5cc799cf047349f2739cd274f1
Gonzalo Cuellar Aliaga <111820928+gonzaliaga@users.noreply.github.com>
2023-11-07 18:44:53 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://gonzalo:Julio1397@cluster0.h1ctdry.mongodb.net/fastfoodapp', 
+mongoose.connect('', 
 /* no olvidarse que hay que pasalo a un ENV para que no se vea 
 {
     useCreateIndex: true,
